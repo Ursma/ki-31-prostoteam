d4900039fde0f1231d0fad7c887398cc63dfff51
Ursma <Bustichyt@gmail.com>
2024-05-22 14:28:05 +0300

    fix

--- src/main/java/com/prisonproject/main/controller/CellController.java
+++ src/main/java/com/prisonproject/main/controller/CellController.java
@@ -17,7 +17,7 @@ public class CellController {
     private final CellService cellService;
 
     @PutMapping("/add")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://127.0.0.1:8081")
     @Operation(summary = "Get all cells")
     public CellEntity addCell(@RequestBody AddCellRequest request){
         return cellService.addCell(request);
--- src/main/java/com/prisonproject/main/controller/InmateController.java
+++ src/main/java/com/prisonproject/main/controller/InmateController.java
@@ -22,35 +22,35 @@ public class InmateController {
     private final InmateService inmateService;
 
     @PutMapping("/add")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://127.0.0.1:8081")
     @Operation(summary = "Додати в'язня", description = "Додає в'язня до камери")
     public InmateEntity addInmateToPrison(@RequestBody AddInmateRequest request){
        return inmateService.addInmateToCell(request);
     }
 
     @PostMapping("/get/full")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://127.0.0.1:8081")
     @Operation(summary = "Отримати повню інформацію про в'язня", description = "Повертає повну інформацію про в'язня")
     public InmateInfoResponse getInmateInfo(@RequestBody GetResponseByName body){
         return inmateService.getInmateResponse(body);
     }
 
     @PostMapping("/get/short")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://127.0.0.1:8081")
     @Operation(summary = "Отримати коротку інформацію про в'язня ", description = "Повертає коротку інформацію про в'язня")
     public InmateInfoWithoutCellResponse getShortGuardInfo(@RequestBody GetResponseByName body){
         return inmateService.getShortInmateInfo(body);
     }
 
     @PostMapping("/get/all")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://127.0.0.1:8081")
     @Operation(summary = "Отримати коротку інформацію про всіх в'язнів ", description = "Повертає коротку інформацію про всіх в'язнів")
     public List<InmateInfoWithoutCellResponse> getShortInfoAboutAllGuards(){
         return inmateService.getShortInfoAboutAllInmates();
     }
 
     @PostMapping("/get/by/cell")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://127.0.0.1:8081")
     @Operation(summary = "Отримати в'язнів по певній камері", description = "Повертає усіх пов'язаних із камерою в'язнів")
     public List<InmateInfoWithoutCellResponse> getAllGuardsByCellId(@RequestBody GetResponseByCellName body){
         return inmateService.getAllInmatesByCell(body);
